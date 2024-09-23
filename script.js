f236b24645a85e3f6c8efe6d29a1c6189e53bdcf
shivam <141661378+shivananadd@users.noreply.github.com>
2024-09-23 17:29:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,23 +45,23 @@ async function checkWeather(city) {
 
   switch (weather_data.weather[0].main) {
     case "Clouds":
-      weather_img.src = "./assets/assets/img/cloud.png";
+      weather_img.src = "./assets/img/cloud.png";
       break;
 
     case "Clear":
-      weather_img.src = "./assets/assets/img/clear.png";
+      weather_img.src = "./assets/img/clear.png";
       break;
 
     case "Rain":
-      weather_img.src = "./assets/assets/img/rain.png";
+      weather_img.src = "./assets/img/rain.png";
       break;
 
     case "Mist":
-      weather_img.src = "./assets/assets/img/mist.png";
+      weather_img.src = "./assets/img/mist.png";
       break;
 
     case "Snow":
-      weather_img.src = "./assets/assets/img/snow.png";
+      weather_img.src = "./assets/img/snow.png";
       break;
   }
 
